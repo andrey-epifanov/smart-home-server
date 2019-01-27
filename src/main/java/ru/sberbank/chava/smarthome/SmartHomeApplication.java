c15c4c2d273242c6e66c9f259d5cbe551fab3614
Андрей <andrey.a.epifanov@gmail.com>
2019-01-27 05:21:39 +0300

    update

--- src/main/java/ru/sberbank/chava/smarthome/SmartHomeApplication.java
+++ src/main/java/ru/sberbank/chava/smarthome/SmartHomeApplication.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
@@ -14,14 +16,16 @@ import java.util.Map;
 public class SmartHomeApplication {
 	private static final Logger log = LoggerFactory.getLogger(SmartHomeApplication.class);
 
-	@Autowired
-	private static JdbcTemplate jdbcTemplate;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartHomeApplication.class, args);
 
+		ApplicationContext ctx
+				= new ClassPathXmlApplicationContext("jdbcBeans.xml");
+
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+
 		List<Map<String, Object>> answer = jdbcTemplate.queryForList("select 1 from dual");
-		log.info("" + answer.size());
+		log.info("Check JDBC connect . True ? = " + answer.size());
 	}
 
 }
